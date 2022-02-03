3b99bf195a7385c0de416d9d30d53e238ab9df4a
vikyjaware <vikyjaware@gmail.com>
2022-02-04 00:14:54 +0530

    [Viky] Add : Folder structure added

--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FunctionalPrograms;
+Console.WriteLine("Welcome to functional problem statement!");
 
-CoinToss tosscoin=new CoinToss();
-tosscoin.toss();
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Enter\n0 for Exit\n" + "1 for flip Coin \n");
+    Console.WriteLine("Enter your option: ");
+    int option = Convert.ToInt32(Console.ReadLine() + "\n");
+
+
+    switch (option)
+    {
+        case 0:
+            flag = false;
+            break;
+
+        case 1:
+            flag = true;
+            CoinToss coin = new CoinToss();
+            coin.toss();
+            break;
+
+    }
+}
