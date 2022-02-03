7c7067033772eb75e8f6727cd51fa32485d90736
vikyjaware <vikyjaware@gmail.com>
2022-02-03 19:29:34 +0530

    [Viky] Add : Folder structure added

---
+++ FunctionalPrograms/CoinToss.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class CoinToss
+    {
+        public void toss()
+        {
+            int head = 0, tail = 0;
+            double toss = 0,PercentHead,PercentTail;
+            Console.WriteLine("Enter the number of times you want to flip the coin ");
+            int num =Convert.ToInt32 (Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+            Random rand = new Random();
+               toss=rand.NextDouble();
+                if(toss<0.5)
+                    head++;
+                else
+                    tail++;
+            }
+            PercentHead =100*head/num;
+            PercentTail =100*tail/num;
+            Console.WriteLine("Perrcentage of head is " + PercentHead + "\nPercentage of tail is " + PercentTail);
+        }
+    }
+}
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using FunctionalPrograms;
+
+CoinToss tosscoin=new CoinToss();
+tosscoin.toss();
