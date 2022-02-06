7d069c231fcae253d6375345e9b1f230699a13ad
vikyjaware <vikyjaware@gmail.com>
2022-02-06 23:20:20 +0530

    [Viky] Add : Folder structure added

---
+++ FunctionalPrograms/PowerOf2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class PowerOf2
+    {
+        public void power()
+        {
+            Console.Write("Enter the value of exponent of 2 ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            Console.WriteLine("The table of power of 2 upto "+value+ " terms is :");
+            Console.WriteLine(result);
+            for (int i = 0; i < value; i++)
+            {
+                result = (result * 2);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -5,7 +5,10 @@ Console.WriteLine("Welcome to functional problem statement!");
 
 
 {
-    Console.WriteLine("Enter 0 for Exit\n" + "Enter 1 for flip Coin problem \n"+"Enter 2 for Leap year problem\n");
+    Console.WriteLine("Enter 0 for Exit\n" 
+        +"Enter 1 for flip Coin problem \n"
+        +"Enter 2 for Leap year problem\n"
+        +"Enter 3 for Power of 2 series");
     Console.WriteLine("Enter your option: ");
     int option = Convert.ToInt32(Console.ReadLine() + "\n");
 
@@ -22,6 +25,10 @@ Console.WriteLine("Welcome to functional problem statement!");
         case 2:
             LeapYear leap=new LeapYear();
             leap.year();
+            break;
+        case 3:
+            PowerOf2 powerOf2=new PowerOf2();
+            powerOf2.power();
             break;
 
     }
